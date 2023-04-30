90f7099297588269edbb8f020f2d0f0ecd6372af
Nolan Flammini <nolantheturtle@gmail.com>
2023-04-30 16:01:17 -0700

    [unknown] netmessage work

--- Network/NetMessage.cs
+++ Network/NetMessage.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using SharpDX.MediaFoundation.DirectX;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -13,46 +16,175 @@ namespace cotf.Network
     public class NetMessage
     {
         internal static NetworkStream Local;
-        internal static NetworkStream[] stream = new NetworkStream[256];
+        internal static NetPlayer[] stream = new NetPlayer[256];
         private static BinaryWriter writer;
         private static BinaryReader reader;
-        protected BinaryReader Reader(int whoAmI) => new BinaryReader(stream[whoAmI]);
-        protected BinaryWriter Writer(int whoAmI) => new BinaryWriter(stream[whoAmI]);
-        internal static void Initialize(IPAddress ip, int port = 8000, byte whoAmI = 0)
+        protected BinaryReader Reader(int whoAmI) => new BinaryReader(stream[whoAmI].Stream);
+        protected BinaryWriter Writer(int whoAmI) => new BinaryWriter(stream[whoAmI].Stream);
+        internal Player instance => Main.myPlayer;
+
+        internal static async void Initialize(IPAddress ip, int port = 8000, byte whoAmI = 0)
         {
             if (Main.netMode == NetModeID.Singleplayer)
                 return;
             if (Main.netMode == NetModeID.MultiplayerClient)
             {
-                var end = new IPEndPoint(ip, port);
+                TcpClient tcp = new TcpClient();
+                UdpClient udp = new UdpClient();
                 //  Primary network handling
-                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                int tries = 0, max = 15;
                 do
                 {
                     try
                     {
-                        Thread.Sleep(1000);
-                        sock.Connect(end);
+                        tcp.Connect(ip, port);
+                        udp.Client = tcp.Client;
                     }
-                    catch { continue; }
-                } while (!sock.Connected);
-                Local = new NetworkStream(sock);
+                    catch 
+                    {
+                        Task.WaitAny(new[] { Task.Delay(1000) });
+                        continue; 
+                    }
+                    Task.WaitAny(new[] { Task.Delay(1000) });
+                } while (!udp.Client.Connected && tries++ < max);
+                Local = new NetworkStream(udp.Client);
                 writer = new BinaryWriter(Local);
                 reader = new BinaryReader(Local);
+                RecieveData();
             }
             if (Main.netMode == NetModeID.Server)
             {
                 //  Begin listen
-                Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                listen.Bind(new IPEndPoint(IPAddress.Any, port));
-                listen.Listen(256);
-                Socket sock = listen.Accept();
-                //  Get whoAmI
-                byte[] buffer = new byte[1];
-                sock.Receive(buffer);
-                byte whoAmi = buffer[0];
+                UdpListener listen = new UdpListener(IPAddress.Any, port);
+                listen.Start(255);
+                UdpClient client = listen.AcceptClient();
+                NetworkStream net = new NetworkStream(client.Client);
+                //  Temporary NetPlayer object
+                NetPlayer player = new NetPlayer()
+                {
+                    Stream = net,
+                    Udp = client
+                };
                 //  Init stream
-                stream[whoAmi] = new NetworkStream(sock);
+                var first = stream.FirstOrDefault(t => t == null);
+                if (first == default)
+                {
+                    //  Either run who-is-connected check and remove disconnected entries
+                    //  or deny connection, server being full
+                    return;
+                }
+                whoAmI = (byte)stream.ToList().IndexOf(first);
+                //  Get player data
+                stream[whoAmI] = await RecieveData(player, whoAmI);
+            }
+        }
+        private static Task<NetPlayer> RecieveData(NetPlayer player, int whoAmI)
+        {
+            return new Task<NetPlayer>(() => 
+            {
+                Wait(player);
+                WaitHandle.WaitAny(new[] { player.wait });
+                if (!player.hasData) return null;
+                //  Give index
+                player.whoAmI = whoAmI;
+                //  Process data
+                //  Load and handle Player object
+                return player;
+            });
+        }
+        private static void Wait(NetPlayer player)
+        {
+            Task.Factory.StartNew(() => 
+            {
+                int tries = 0, max = 30;
+                while (player.Connected && tries++ < max)
+                {
+                    Task.WaitAll(new[] {Task.Delay(1000)});
+                    if (player.Stream.DataAvailable)
+                    {
+                        player.hasData = true;
+                        player.wait.Set();
+                    }
+                }
+                player.hasData = false;
+                player.wait.Set();
+            });
+        }
+        private static void HandlePackets()
+        {
+            Task.Factory.StartNew(() => 
+            { 
+                if (Main.netMode == NetModeID.Singleplayer)
+                    return;
+                while (Local.Socket.Connected)
+                {
+                    if (!Local.DataAvailable)
+                    {
+                        Task.WaitAll(new[] { Task.Delay(5) });
+                        continue;
+                    }
+                    Packet get = HandlePacket();
+                    
+                }
+            });
+        }
+        private static Packet HandlePacket()
+        {
+            Packet packet = new Packet();
+            long start = 0;
+            int tries = 0, max = 10;
+            while (tries++ < max)
+            {
+                try
+                {
+                    start = reader.ReadInt64();
+                    break;
+                }
+                catch 
+                {
+                    continue;
+                }
+            }
+            if (start == -100L)
+            { 
+                packet.packet = reader.ReadByte();
+                packet.num  = reader.ReadInt32();
+                packet.num2 = reader.ReadInt32();
+                packet.num3 = reader.ReadInt32();
+                return packet;
+            }
+            return null;
+        }
+        private static void RecieveData(Packet packet, int toWhom = -1, int fromWhom = -1, NetInfo info = null, int num = 0, int num2 = 0, int num3 = 0)
+        {
+            if (Main.netMode == NetModeID.Singleplayer)
+                return;
+            if (Main.netMode == NetModeID.MultiplayerClient)
+            {
+                switch (packet)
+                {
+
+                }
+            }
+            if (Main.netMode == NetModeID.Server)
+            {
+
+            }
+        }
+        private static void SendData(NetPlayer player, byte packet, int toWhom = -1, int fromWhom = -1, NetInfo info = null, int num = 0, int num2 = 0, int num3 = 0)
+        {
+            if (Main.netMode == NetModeID.Singleplayer)
+                return;
+            if (Main.netMode == NetModeID.MultiplayerClient)
+            {
+                switch (packet)
+                {
+
+                }
+            }
+            if (Main.netMode == NetModeID.Server)
+            {
+
             }
         }
         public static void SendData(byte packet, int toWhom = -1, int fromWhom = -1, NetInfo info = null, int num = 0, int num2 = 0, int num3 = 0)
@@ -77,18 +209,42 @@ namespace cotf.Network
         public Packet()
         {
         }
+        public const long 
+            START = -100;
+        public byte packet;
+        public int num, num2, num3;
         private BinaryWriter write;
         public void Send(byte packet, int toWhom = 255, NetInfo info = null)
         {
             if (NetMessage.stream[toWhom] == null)
                 return;
-            write = new BinaryWriter(NetMessage.stream[toWhom]);
+            write = new BinaryWriter(NetMessage.stream[toWhom].Stream);
             write.Write(packet);
         }
     }
-
+    public class PacketID
+    {
+        public const byte
+            JoinWorld = 0;
+    }
+    public class NetPlayer
+    {
+        internal bool hasData;
+        internal bool Available => Stream.DataAvailable;
+        public bool Connected => Udp.Client.Connected;
+        public int whoAmI
+        {
+            get { return instance.whoAmI; }
+            set { instance.whoAmI = value; }
+        }
+        public UdpClient Udp { get; internal set; }
+        public NetworkStream Stream { get; internal set; }
+        public AutoResetEvent wait = new AutoResetEvent(true);
+        public Player instance;
+    }
     public class NetInfo
     {
+        public Packet packet;
     }
     public sealed class NetMessageID
     {
@@ -101,5 +257,24 @@ namespace cotf.Network
             Singleplayer = 0,
             MultiplayerClient = 1,
             Server = 2;
+    }
+    public sealed class UdpListener
+    {
+        TcpListener listen;
+        ~UdpListener() { }
+        public UdpListener(IPAddress ip, int port)
+        {
+            listen = new TcpListener(ip, port);
+        }
+        public void Start(int backlog)
+        {
+            listen.Start(backlog);
+        }
+        public UdpClient AcceptClient()
+        {
+            UdpClient client = new UdpClient();
+            client.Client = listen.AcceptSocket();
+            return client;
+        }
     }
 }
